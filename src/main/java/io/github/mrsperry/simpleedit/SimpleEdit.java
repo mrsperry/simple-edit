45cabf767ea4ba3660a55960b63c7dd90f996146
Josh <mrjoshuasperry@gmail.com>
2021-03-11 07:13:33 -0500

    Initial commit

---
+++ src/main/java/io/github/mrsperry/simpleedit/SimpleEdit.java
@@ -0,0 +1,10 @@
+package io.github.mrsperry.simpleedit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleEdit extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
