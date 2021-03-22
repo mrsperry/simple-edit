265f8d75e436d42caebc4475084de8aae052ef70
Josh <mrjoshuasperry@gmail.com>
2021-03-22 05:57:33 -0400

    Fix fill action by using a breadth-first search

--- src/main/java/io/github/mrsperry/simpleedit/sessions/actions/FillAction.java
+++ src/main/java/io/github/mrsperry/simpleedit/sessions/actions/FillAction.java
@@ -1,28 +1,72 @@
 package io.github.mrsperry.simpleedit.sessions.actions;
 
+import com.google.common.collect.Lists;
 import io.github.mrsperry.mcutils.classes.Pair;
 import io.github.mrsperry.simpleedit.sessions.selections.SelectionHistory;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.util.Vector;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public final class FillAction extends Action {
     private FillAction(final SelectionHistory history, final Location center, final int radius, final List<Pair<Material, Integer>> materials) {
+        final World world = center.getWorld();
+        if (world == null) {
+            return;
+        }
+
+        final Vector centerVector = center.toVector();
         final List<Block> blocks = new ArrayList<>();
-        for (int x = -radius; x <= radius; x++) {
-            for (int z = -radius; z <= radius; z++) {
-                final Location location = center.clone().add(x, 0, z);
+        final Queue<Block> frontier = new PriorityQueue<>();
+        frontier.add(world.getBlockAt(center));
 
-                for (int y = location.getBlockY(); y >= 0; y--) {
-                    final Block current = location.subtract(0, location.getBlockY() - y, 0).getBlock();
-                    if (current.getType().isSolid()) {
-                        break;
-                    }
+        // Breadth-first search
+        while (!frontier.isEmpty()) {
+            final Block current = frontier.poll();
 
-                    blocks.add(current);
+            final List<Block> neighbors = Lists.newArrayList(
+                    current.getRelative(BlockFace.NORTH),
+                    current.getRelative(BlockFace.SOUTH),
+                    current.getRelative(BlockFace.EAST),
+                    current.getRelative(BlockFace.WEST)
+            );
+
+            for (final Block block : neighbors) {
+                if (blocks.contains(block)) {
+                    continue;
                 }
+
+                if (block.getType().isSolid()) {
+                    continue;
+                }
+
+                if (block.getLocation().toVector().distance(centerVector) > radius) {
+                    continue;
+                }
+
+                blocks.add(block);
+                frontier.add(block);
+            }
+        }
+
+        // Add all non-solid blocks below the current blocks
+        for (final Block block : blocks) {
+            final int yLevel = block.getY();
+
+            for (int y = yLevel - 1; y >= 0; y--) {
+                final Block current = block.getRelative(0, -(yLevel - y), 0);
+
+                if (current.getType().isSolid()) {
+                    break;
+                }
+
+                blocks.add(current);
             }
         }
 
